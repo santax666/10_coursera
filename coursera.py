d0b9f26432e05a04f739750b12cff815b695747c
Your Name1 <you1@example.com>
2016-10-03 23:56:47 +0300

    ...

--- coursera.py
+++ coursera.py
@@ -1,8 +1,17 @@
+import requests
+from lxml import etree
 
 
-def get_courses_list():
-    pass
+def get_site_data(url):
+    site_data = requests.get(url)
+    return site_data.content
 
+
+def get_courses_list(data):
+    notes = etree.fromstring(data)
+
+    for field in notes.findall('.//url'):
+        print('Note to:',field.text)
 
 def get_course_info(course_slug):
     pass
@@ -14,3 +23,7 @@ def output_courses_info_to_xlsx(filepath):
 
 if __name__ == '__main__':
     pass
+    url = 'https://www.coursera.org/sitemap~www~courses.xml'
+    site_data = get_site_data(url)
+    courses_list = get_courses_list(site_data)
+    
